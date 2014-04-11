c03b03facc995d0581f0f0f5103e97753831b5da
zain93 <zainyj@hotmail.com>
2014-04-12 00:56:22 +0100

    updated lateral movement

--- LateralMovement.js
+++ LateralMovement.js
@@ -27,6 +27,27 @@ function Start ()
 	moveSpeed = 3.0;
 }
 
-function Update () {
+function Update () 
+{
+	if (suspended)
+	{	
+		return;
+	}
+	if (Input.GetButton(leftButton))
+	{
+		transform.rotation.y = leftRotation;
+		transform.Translate(forward * moveSpeed * dt);
+		animation.Play("walk");
+	}
+	else if (Input.GetButton(rightButton))
+	{
+		transform.rotation.y = rightRotation;
+		transform.Translate(forward * moveSpeed * dt);
+		animation.Play("walk");	
+	}
 
+}
+function SetSuspension(setting : boolean)
+{
+	suspended = setting;
 }
